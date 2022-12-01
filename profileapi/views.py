e11120339955d05e4621c88e68f7ab0cd6c8ccad
Rehan Ali Nawab <nawabrehanali1@gmail.com>
2022-12-01 09:29:08 +0530

    Creating serializers for the POST request

--- profileapi/views.py
+++ profileapi/views.py
@@ -2,8 +2,11 @@
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profileapi import serializers
 
 class HelloApiVIew(APIView):
+    serializer_class = serializers.HelloSerializers
 
     def get(self, request, format=None):
         an_api = [
@@ -12,4 +15,18 @@ class HelloApiVIew(APIView):
         "Gives you control over ur application and logic",
         "It mapped manually to URL's"
         ]
+        print("Log for ans_api",type(an_api))
         return Response({"message":"hello world", "an_api" : an_api})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        print("Log for serializer", type(serializer))
+        if serializer.is_valid():
+            name=serializer.validated_data.get("name")
+            age=serializer.validated_data.get("age")
+            message = f"hello {name, age}"
+            return Response({'message' : message})
+        else:
+            return Response(serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
